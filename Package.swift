bc222fa4133faded459cc454d122449d1184d242
Jan de Boer <44832123+Janldeboer@users.noreply.github.com>
2023-09-13 16:20:27 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,19 +1,17 @@
-// swift-tools-version: 5.8
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "PDFKitSwiftUI",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v13)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "PDFKitSwiftUI",
             targets: ["PDFKitSwiftUI"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "PDFKitSwiftUI"),
         .testTarget(
