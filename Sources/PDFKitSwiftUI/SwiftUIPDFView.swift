9bc3ec7c21380cda93ad84d6ff89bf7679edc05a
Jan de Boer <44832123+Janldeboer@users.noreply.github.com>
2023-09-13 16:11:51 +0200

    Update SwiftUIPDFView.swift

--- Sources/PDFKitSwiftUI/SwiftUIPDFView.swift
+++ Sources/PDFKitSwiftUI/SwiftUIPDFView.swift
@@ -1,6 +1,11 @@
 import SwiftUI
 import PDFKit
 
+import SwiftUI
+import PDFKit
+
+#if os(iOS)
+
 @available(iOS 13.0, *)
 public struct SwiftUIPDFView: UIViewRepresentable {
     var pdfDocument: PDFDocument
@@ -20,3 +25,24 @@ public struct SwiftUIPDFView: UIViewRepresentable {
     }
 }
 
+#elseif os(macOS)
+
+public struct SwiftUIPDFView: NSViewRepresentable {
+    var pdfDocument: PDFDocument
+
+    public init(pdfDocument: PDFDocument) {
+        self.pdfDocument = pdfDocument
+    }
+
+    public func makeNSView(context: Context) -> PDFView {
+        let pdfView = PDFView()
+        pdfView.document = pdfDocument
+        return pdfView
+    }
+
+    public func updateNSView(_ pdfView: PDFView, context: Context) {
+        pdfView.document = pdfDocument
+    }
+}
+
+#endif
