be373d3888733f259dedc5db200faaa395982ad5
Jan de Boer <44832123+Janldeboer@users.noreply.github.com>
2023-09-13 16:12:08 +0200

    Update SwiftUIPDFThumbnailView.swift

--- Sources/PDFKitSwiftUI/SwiftUIPDFThumbnailView.swift
+++ Sources/PDFKitSwiftUI/SwiftUIPDFThumbnailView.swift
@@ -1,6 +1,8 @@
 import SwiftUI
 import PDFKit
 
+#if os(iOS)
+
 @available(iOS 13.0, *)
 public struct SwiftUIPDFThumbnailView: UIViewRepresentable {
     @Binding var pdfDocument: PDFDocument
@@ -21,3 +23,25 @@ public struct SwiftUIPDFThumbnailView: UIViewRepresentable {
     }
 }
 
+#elseif os(macOS)
+
+public struct SwiftUIPDFThumbnailView: NSViewRepresentable {
+    @Binding var pdfDocument: PDFDocument
+
+    public init(pdfDocument: Binding<PDFDocument>) {
+        self._pdfDocument = pdfDocument
+    }
+
+    public func makeNSView(context: Context) -> PDFThumbnailView {
+        let thumbnailView = PDFThumbnailView()
+        thumbnailView.thumbnailSize = CGSize(width: 50, height: 50)
+        thumbnailView.layoutMode = .horizontal
+        return thumbnailView
+    }
+
+    public func updateNSView(_ thumbnailView: PDFThumbnailView, context: Context) {
+        thumbnailView.pdfView?.document = pdfDocument
+    }
+}
+
+#endif
